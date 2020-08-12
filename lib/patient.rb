a5ec8c1b00db1ed214cf376b19e79e8bfda0efd1
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-12 04:14:40 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,18 +8,21 @@ def initialize(name)
   @name = name
   @@all << self 
 end 
-  
-def new_appointment
-end 
-  
+
 def self.all
     @@all 
 end 
   
+def new_appointment(date, doctor)
+  Appointment.new(date, self, doctor)
+end 
+  
 def appointments
+  
 end 
   
 def doctors
+  Patient.map(&:doctor)
 end 
   
 end 
